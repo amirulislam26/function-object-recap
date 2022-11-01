f59908ee4672b34af29bf2444a73e4a460164f0c
amirulislam26 <26techamirulislam@gmail.com>
2022-11-01 01:40:02 -0700

    my love story if else provlem solved

---
+++ C/thiree.js
@@ -0,0 +1,27 @@
+const amrLove = 100;
+const yorLove = 99;
+
+if(amrLove < yorLove){
+    console.log('ami tore valobasi')
+}
+else{
+    console.log('ami tomoy valovasi na')
+}
+if(amrLove > yorLove){
+    console.log('ami tore valobasi')
+}
+else{
+    console.log('ami tomoy valovasi na')
+}
+if(amrLove >= yorLove){
+    console.log('ami tore valobasi')
+}
+else{
+    console.log('ami tomoy valovasi na')
+}
+if(amrLove <= yorLove){
+    console.log('ami tore valobasi')
+}
+else{
+    console.log('ami tomoy valovasi na keno bar bar boltaco')
+}
