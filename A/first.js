7f5802f5f71a551825191f45eed777a83b76cdd1
amirulislam26 <26techamirulislam@gmail.com>
2022-11-01 01:22:17 -0700

    added a first provlem solved

---
+++ A/first.js
@@ -0,0 +1,15 @@
+ function getFactorial(number){
+     let factorial = 1;
+     for(let i = 1; i <= number; i++){
+         factorial = factorial * i;
+         console.log(i, factorial);
+     }
+     return factorial;
+}
+
+ const  factorial = getFactorial(8);
+ console.log('my Love Factorial:', factorial);
+
+if(false){
+    console.log(false)
+}
